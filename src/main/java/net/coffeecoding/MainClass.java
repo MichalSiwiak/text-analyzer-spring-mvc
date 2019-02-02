f00c4485d836dbd1e1879d587f2b0b871920e175
siwiakmichal <siwiakmichal@gmail.com>
2019-02-02 02:13:06 +0100

    up

--- src/main/java/net/coffeecoding/MainClass.java
+++ src/main/java/net/coffeecoding/MainClass.java
@@ -7,10 +7,8 @@ public class MainClass {
     public static void main(String[] args) throws IOException {
 
         Map<String, Integer> testMap = new HashMap<>();
-
-
         BufferedReader br = new BufferedReader(new InputStreamReader(
-                new FileInputStream("C:\\Users\\msiwiak\\Downloads\\test.txt"), "UTF-8"));
+                new FileInputStream("/home/michal/Pulpit/test.txt"), "UTF-8"));
         String line;
         while ((line = br.readLine()) != null) {
             wordCounter(line, testMap);
@@ -18,37 +16,42 @@ public class MainClass {
 
         br.close();
 
-
         System.out.println(sortByValue(testMap).toString());
 
     }
 
 
     private static Map<String, Integer> wordCounter(String string, Map<String, Integer> wordsMap) {
+
         Pattern pattern = Pattern.compile("[^A-Za-z0-9]");
         String[] words = string.split(" ");
 
         for (String word : words) {
 
-            int first = 0;
-            int last = 0;
+            int headNonAlphanumeric = 0;
+            int tailNonAlphanumeric = 0;
+
 
             //removing non alphanumeric characters from head
             for (int i = 0; i < word.length(); i++) {
                 boolean check = pattern.matcher(String.valueOf(word.charAt(i))).matches();
-                if (check) first = i + 1;
+                if (check) headNonAlphanumeric = i + 1;
                 else break;
             }
-            if (first > 0) word = word.substring(first);
+            if (headNonAlphanumeric > 0) word = word.substring(headNonAlphanumeric);
+
 
             //removing non alphanumeric characters from tail
             for (int i = word.length() - 1; i >= 0; i--) {
                 boolean check = pattern.matcher(String.valueOf(word.charAt(i))).matches();
-                if (check) last = i;
+                if (check) tailNonAlphanumeric = i;
                 else break;
             }
+
+            if (tailNonAlphanumeric > 0) word = word.substring(0, tailNonAlphanumeric);
+
+
             //filling map and count words
-            if (last > 0) word = word.substring(0, last);
             if (wordsMap.containsKey(word) && word.length() > 0) {
                 wordsMap.put(word, wordsMap.get(word) + 1);
             } else if (word.length() > 0) {
